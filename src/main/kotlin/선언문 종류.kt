c94151bd5a0f1411377ada1a501759a3a6cfb2f0
Oh-JunTaek <aa39043533@gmail.com>
2023-09-19 19:14:42 +0900

    선언문 종류

---
+++ src/main/kotlin/선언문 종류.kt
@@ -0,0 +1,20 @@
+String //문자열을 표현하는 타입입니다. 큰따옴표(")로 둘러싸서 문자열 리터럴을 생성할 수 있습니다.
+val greeting: String = "Hello, World!"
+
+Int //정수를 표현하는 타입입니다. 32비트의 범위(-2,147,483,648 ~ 2,147,483,647)를 가집니다.
+val age: Int = 30
+
+Double //부동소수점 숫자를 표현하는 타입입니다. 64비트의 범위와 높은 정밀도를 가집니다.
+val pi: Double = 3.141592653589793
+
+Char //단일 문자를 표현하는 타입입니다. 작은따옴표(')로 둘러싸서 문자 리터럴을 생성할 수 있습니다.
+val letter: Char = 'A'
+
+Boolean //참(true) 또는 거짓(false) 값을 표현하는 타입입니다.
+val isAdult: Boolean = true
+
+Float //부동소수점 숫자를 표현하는 타입입니다만 Double보다 낮은 정밀도(32비트)를 가집니다.
+val smallPi: Float = 3.14f // f or F 접미사가 필요합니다.
+
+Long //매우 큰 정수를 표현하는 타입으로 Int보다 큰 범위(64비트)를 가집니다.
+val largeNumber: Long = 10000000000L // L 접미사가 필요합니다.
